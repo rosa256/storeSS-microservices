08c0d76a7f3f75196b12f31e12f23cf2bf1517e7
Damian Rosinski <d.rosinski256@gmail.com>
2020-09-24 22:30:01 +0200

    Added services registry based on eureka server

---
+++ services-registry/src/main/java/com/sample/store/servicesregistry/ServicesRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.sample.store.servicesregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesRegistryApplication.class, args);
+	}
+
+}
