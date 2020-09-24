5d5f0fd73b26558fa991bd16b26e18d585a46125
Damian Rosinski <d.rosinski256@gmail.com>
2020-09-24 22:41:21 +0200

    Added users service

---
+++ users-service/src/test/java/com/sample/store/usersservice/UsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.store.usersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
