70d1da3bfc31e5e70e15586c54d7c036a97494f7
Damian Rosinski <d.rosinski256@gmail.com>
2020-09-24 22:25:43 +0200

    Added zull gateway for microservices

---
+++ services-gateway/src/test/java/com/sample/store/servicesgateway/ServicesGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.store.servicesgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
