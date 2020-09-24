71bd5b8b5a2f397f9d17ab88b6639de4be7afad5
Damian Rosinski <d.rosinski256@gmail.com>
2020-09-24 22:43:37 +0200

    Enabled eureka client for services

--- orders-service/src/main/java/com/sample/store/ordersservice/OrdersServiceApplication.java
+++ orders-service/src/main/java/com/sample/store/ordersservice/OrdersServiceApplication.java
@@ -2,8 +2,10 @@ package com.sample.store.ordersservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrdersServiceApplication {
 
 	public static void main(String[] args) {
