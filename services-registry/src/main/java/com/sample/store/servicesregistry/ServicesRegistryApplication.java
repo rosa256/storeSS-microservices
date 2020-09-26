8d3bdacbb70a299beee77610d03984ba53252a9c
Damian <d.rosinski256@gmail.com>
2020-09-27 00:11:10 +0200

    Enabled eurekaServer and missing dependencies

--- services-registry/src/main/java/com/sample/store/servicesregistry/ServicesRegistryApplication.java
+++ services-registry/src/main/java/com/sample/store/servicesregistry/ServicesRegistryApplication.java
@@ -2,8 +2,10 @@ package com.sample.store.servicesregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServicesRegistryApplication {
 
 	public static void main(String[] args) {
