3500dd91f881e42d8b33cc0441d04bfa0812becc
Damian <d.rosinski256@gmail.com>
2020-09-27 16:13:00 +0200

    Added Images service

---
+++ images-service/src/main/java/com/sample/store/imagesservice/ImagesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sample.store.imagesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagesServiceApplication.class, args);
+	}
+
+}
---
+++ images-service/src/test/java/com/sample/store/imagesservice/ImagesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.store.imagesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
