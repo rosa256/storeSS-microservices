a45a5485c5560810c11a1ec48e6ffb9a2c35eabc
Damian <d.rosinski256@gmail.com>
2020-09-27 16:18:30 +0200

    images service - updated configuration

--- images-service/src/main/java/com/sample/store/imagesservice/ImagesServiceApplication.java
+++ images-service/src/main/java/com/sample/store/imagesservice/ImagesServiceApplication.java
@@ -2,8 +2,11 @@ package com.sample.store.imagesservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ImagesServiceApplication {
 
 	public static void main(String[] args) {
