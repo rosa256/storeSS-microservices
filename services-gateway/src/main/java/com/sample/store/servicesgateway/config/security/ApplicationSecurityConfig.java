1e5fda93a04a5866f7e3652660d7b15f7e341835
Damian Rosinski <d.rosinski256@gmail.com>
2020-11-14 11:57:14 +0100

    Pushing changes - formating PC

--- services-gateway/src/main/java/com/sample/store/servicesgateway/config/security/ApplicationSecurityConfig.java
+++ services-gateway/src/main/java/com/sample/store/servicesgateway/config/security/ApplicationSecurityConfig.java
@@ -32,7 +32,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         http
             .csrf().disable()
             .authorizeRequests()
-            .antMatchers("/basicauth").permitAll()
+            .antMatchers( "/api/**", "/").permitAll()
             .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
             .anyRequest().authenticated()
             .and()
